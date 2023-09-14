7ed0e72c966911df48214944edbb6fbe951422a6
tommasoagudio <tommaso_agudio@hotmail.com>
2023-09-14 16:15:08 +0200

    Removed useless files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Desktop cleaner",
-    version="0.1",
-    description="simple desktop cleaner that moves all files from desktop to a folder with the current date and time",
-    executables=[Executable("main.py")]
-)
